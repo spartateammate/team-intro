06a6c2dbf1ed6610b0d660c6615d167531601baa
박가온누리 <gaonnuri@baggaonnuliui-MacBookAir.local>
2024-08-29 11:16:58 +0900

    all arch

--- js/member.js
+++ js/member.js
@@ -1,8 +1,10 @@
 const members = [
     {
         name: '박가온누리',
-        title: '든든한 팀장',
-        description: '든든한 당신이 있어서 행운이에요!',
+        short: 'Java 뿌시겠습니다!!',
+        MBTI: 'MBTI: INFP',
+        Strength: '장점: 기본 이론을 베이스로 응용력이 좋고, 기획력이 뛰어나다.',
+        GroupPlay: '협업 스타일: 팀원들과 꾸준히 진척도를 공유하기',
         imageUrl: 'assets/image/박가온누리.jpeg',
         githubUrl: 'https://github.com/worldcenter96',
         blogType: 'tistory',
@@ -10,8 +12,10 @@ const members = [
     },
     {
         name: '김명훈',
-        title: '.',
-        description: '.',
+        short: '잘부탁드립니다',
+        MBTI: 'MBTI: ENFJ',
+        Strength: '장점: 실패를 두려워 하지 않는 무한 긍정 마인드와 스트레스를 빠르게 극복하는 회복탄력성.',
+        GroupPlay: '협업 스타일: 팀으로 묶인 이상 낙오는 없다. 내가 부족한 건 노력을 들여서 더 공부하고, 어려움에 처한 팀원은 이끌어서 함께 가자.',
         imageUrl: 'assets/image/김명훈.jpg',
         githubUrl: 'https://github.com/anewsdev',
         blogType: 'tistory',
@@ -19,8 +23,10 @@ const members = [
     },
     {
         name: '허원경',
-        title: '.',
-        description: '.',
+        short: '잘 배워보겠습니다ㅎㅎ..',
+        MBTI: 'MBTI: ISFP',
+        Strength: '장점: 탁월한 끈기와 인내심, 그리고 한가지에 높은 집중력',
+        GroupPlay: '협업 스타일: 팀의 필요에 맞춰 유연하게 조정하고, 효율적으로 조화를 이루기.',
         imageUrl: 'assets/image/허원경.png',
         githubUrl: 'https://github.com/hwk9407',
         blogType: 'velog',
@@ -28,8 +34,10 @@ const members = [
     },
     {
         name: '류지수',
-        title: '.',
-        description: '.',
+        short: 'solution 함수가 제일 싫었어요.',
+        MBTI: 'MBTI: INTP',
+        Strength: '장점: 한번 앉으면 목표한 만큼 해내기 전까지 일어나지 않는다.',
+        GroupPlay: '협업 스타일: 모르는 것은 먼저 물어보고, 아는 것은 먼저 말해주면서 서로서로 윈윈할 수 있는 스타일',
         imageUrl: 'assets/image/류지수.jpg',
         githubUrl: 'https://github.com/dameun0527',
         blogType: 'velog',
@@ -37,8 +45,10 @@ const members = [
     },
     {
         name: '민지형',
-        title: '.',
-        description: '.',
+        short: '열심히 할께요.. ㅇ.<',
+        MBTI: 'MBTI: ISFJ',
+        Strength: '장점: 고집이 센 편이라 어떻게든 해결해보려고 시도함',
+        GroupPlay: '협업 스타일: 부족한 부분은 최대한 따라가려고 노력하기.',
         imageUrl: 'assets/image/민지형.jpg',
         githubUrl: 'https://github.com/minjh12',
         blogType: 'velog',
@@ -50,7 +60,10 @@ const members = [
 const modal = document.getElementById("teamMemberModal");
 const modalTitle = document.getElementById("modalTitle");
 const modalImage = document.getElementById("modalImage");
-const modalDescription = document.getElementById("modalDescription");
+const modalShort = document.getElementById("modalShort");
+const modalMBTI = document.getElementById("modalMBTI");
+const modalStrength = document.getElementById("modalStrength");
+const modalgroupPlay = document.getElementById("modalgroupPlay");
 const githubButton = document.getElementById("githubButton");
 const blogButtons = document.getElementById("blogButtons");
 
@@ -61,7 +74,11 @@ function openModal(index) {
     // 모달에 팀원 정보 채우기
     modalTitle.textContent = member.name;
     modalImage.src = member.imageUrl;
-    modalDescription.textContent = member.description;
+    modalShort.textContent = member.short;
+    modalMBTI.textContent = member.MBTI;
+    modalStrength.textContent = member.Strength;
+    modalgroupPlay.textContent = member.GroupPlay;
+
 
     // GitHub 버튼 링크 설정
     githubButton.onclick = function () {
